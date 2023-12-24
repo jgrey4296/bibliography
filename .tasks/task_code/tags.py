8feb23574d4bdb1049284b35abab15046fc50328
John Grey <johngrey4296@gmail.com>
2023-12-24 16:58:21 +0000

    [add]: tags tasks

---
+++ .tasks/task_code/tags.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""
+
+
+See EOF for license/metadata/notes as applicable
+"""
+
+##-- builtin imports
+from __future__ import annotations
+
+# import abc
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+import weakref
+# from copy import deepcopy
+# from dataclasses import InitVar, dataclass, field
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
+                    Iterable, Iterator, Mapping, Match, MutableMapping,
+                    Protocol, Sequence, Tuple, TypeAlias, TypeGuard, TypeVar,
+                    cast, final, overload, runtime_checkable, Generator)
+from uuid import UUID, uuid1
+
+##-- end builtin imports
+
+##-- lib imports
+import more_itertools as mitz
+##-- end lib imports
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+import doot
+import doot.errors
+from doot.structs import DootKey
+from dootle.tags.structs import TagFile, SubstitutionFile
+
+UPDATE_KEY = DootKey("update_")
+FROM_KEY   = DootKey("from")
+TO_KEY = DootKey("to")
+
+def read_tags(spec, state):
+    update = UPDATE_KEY.redirect(spec)
+    target = FROM_KEY.to_path(spec, state)
+
+    tags = TagFile.read(target)
+
+    return { update : tags }
+
+
+class ReadSubs:
+    _total = SubstitutionFile()
+
+    def __call__(self, spec, state):
+        target  = FROM_KEY.to_path(spec, state)
+
+        target_subs = SubstitutionFile.read(target)
+        ReadSubs._total.update(target_subs)
+
+
+def write_known(spec, state):
+    target  = TO_KEY.to_path(spec, state)
+    total   = ReadSubs._total.to_set()
+    target.write_text("\n".join(sorted(total)))
+
+def write_new(spec, state):
+    total   = FROM_KEY.to_type(spec, state, type_=TagFile)
+    known   = ReadSubs._total
+
+    new_tags = {x for x in total if not (known.has_sub(x) or x in known)}
+
+    target  = TO_KEY.to_path(spec, state)
+    target.write_text("\n".join(sorted(new_tags)))
+
+
+
+"""
+
+
+"""
