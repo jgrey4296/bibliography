7fd08884151ac29bd471bd54a4bf8d2abc4ea1eb
John Grey <johngrey4296@gmail.com>
2025-03-20 02:02:27 +0000

    [chore]:

--- .tasks/taskcode/io_stack.py
+++ .tasks/taskcode/io_stack.py
@@ -70,65 +70,76 @@ logging = logmod.getLogger(__name__)
 sort_firsts = ["title", "author", "editor", "year", "tags", "booktitle", "journal", "volume", "number", "edition", "edition_year", "publisher"]
 sort_lasts  = ["isbn", "doi", "url", "file", "crossref"]
 sub_fields  = ["publisher", "journal", "series", "institution"]
-meta_keys   = {"all", "apply", "latex", "rst", "subs", "check", "fsort", "esort", "count", "names"}
+meta_keys   = {
+    "all"    : "run all middlewares",
+    "check"  : "check metadata/urls",
+    "latex"  : "latex read/write",
+    "rst"    : "rst write",
+    "count"  : "field counting",
+    "online" : "online downloading",
+    "format" : "general formatting",
+    "validate" : "in depth validation / metadata application"
+    "report"  : "generate a report on the loaded bibtex",
+}
 
 # Body:
+
 @DKeyed.kwargs
 @DKeyed.paths("lib-root")
+@DKeyed.paths("online_saves")
 @DKeyed.types("people_subs", "tag_subs", "other_subs", check=SubstitutionFile|None)
 @DKeyed.redirects("update_")
-def build_new_stack(spec, state, kwargs:dict, _libroot:pl.Path, _namesubs:M_SubF=None, _tagsubs:M_SubF=None, _othersubs:M_SubF=None, _update=None) -> PairStack:
+def build_new_stack(spec, state, kwargs:dict, _libroot:pl.Path, _online:pl.Path, _namesubs:M_SubF=None, _tagsubs:M_SubF=None, _othersubs:M_SubF=None, _update=None) -> PairStack:
     """ Build a new PairStack of middlewares, with optional and required elements
     Because of how pairstack works, to see the parse stack, read from top to bottom.
     To see the write transforms, read from bottom to top.
     """
     _meta = set(x for x,y in kwargs.items() if y is True)
-    if bool((extra:=_meta - meta_keys)):
+    if bool((extra:=_meta - set(meta_keys.keys()))):
         msg = "Unrecognised meta keys provided"
         raise ValueError(msg, extra)
 
-    ALL   = "all" in _meta
-    # Files
-    APPLY = ALL or "apply" in _meta
-    CHECK = ALL or "check" in _meta
-    # Export
-    LATEX = ALL or "latex" in _meta
-    # General formatting
-    FSORT = ALL or "fsort" in _meta
-    ESORT = ALL or "esort" in _meta
-    # Meta
-    COUNT = ALL or "count" in _meta
-    # Correction
-    SUBS  = ALL or "subs" in _meta
-    NAMES = ALL or "names" in _meta
+    ALL    = "all" in _meta
+    CHECK  = ALL or "check" in _meta
+    LATEX  = ALL or "latex" in _meta
+    RST    = ALL or "rst"   in _meta
+    COUNT  = ALL or "count" in _meta
+    ONLINE = ALL or "online" in _meta
+    FORMAT = ALL or "format" in _meta
+    REPORT = ALL or "reprot" in _meta
 
-    stack = PairStack()
+    stack  = PairStack()
     # Very first/last middlewares:
-    #
-    stack.add(read=[BM.failure.DuplicateKeyHandler(),
-                    ],
+    stack.add(read=[BM.failure.DuplicateKeyHandler()],
               write=[
                   BM.failure.FailureHandler(),
-                  BM.metadata.ApplyMetadata() if APPLY else None,
+                  BM.metadata.ApplyMetadata() if VALIDATE else None,
               ])
-    # Add bidirectional transforms
+    # Add standard bidirectional transforms
     stack.add(BM.bidi.BraceWrapper(),
               BM.bidi.BidiLatex() if LATEX else None,
               BM.bidi.BidiPaths(lib_root=_libroot),
-              BM.bidi.BidiNames(parts=True, authors=True) if NAMES else None,
-              BM.bidi.BidiIsbn(),
-              BM.bidi.BidiTags(),
               None,
-              read=[
-                  BM.metadata.KeyLocker(),
-                  BM.fields.TitleSplitter()
-              ],
-              write=[
-                  BM.fields.FieldSorter(first=sort_firsts, last=sort_lasts) if FSORT else None,
-                  BM.metadata.EntrySorter() if ESORT else None,
-              ])
+              )
 
-    if COUNT:
+    if FORMAT:   # Cleaning up entries
+        stack.add(
+            BM.bidi.BidiNames(parts=True, authors=True),
+            BM.bidi.BidiIsbn(),
+            BM.bidi.BidiTags(),
+            None,
+            read=[
+                BM.metadata.KeyLocker(),
+                BM.fields.TitleSplitter()
+            ]
+            write=[
+                BM.fields.FieldSorter(first=sort_firsts, last=sort_lasts),
+                BM.metadata.EntrySorter(),
+                BM.fields.FieldSubstitutor(fields=["tags"], subs=_tagsubs) if _tagsubs is not None else None,
+                BM.fields.FieldSubstitutor(fields=sub_fields, subs=_othersubs, force_single_value=True) if _othersubs is not None else None,
+            ])
+
+    if FORMAT and COUNT: # counting tags etc
         # Accumulate various fields
         stack.add(write=[
             BM.fields.FieldAccumulator(name="all-tags",     fields=["tags"]),
@@ -140,21 +151,31 @@ def build_new_stack(spec, state, kwargs:dict, _libroot:pl.Path, _namesubs:M_SubF
             # BM.fields.FieldDifference(known=_tagsubs, accumulated="all-tags")
         ])
 
-    if SUBS:
-        stack.add(write=[
-            # NameSubs need to merge with BidiNames
-            # BM.people.NameSubstitutor(_namesubs) if _namesubs is not None else None,
-            BM.fields.FieldSubstitutor(fields=["tags"], subs=_tagsubs) if _tagsubs is not None else None,
-            BM.fields.FieldSubstitutor(fields=sub_fields, subs=_othersubs, force_single_value=True) if _othersubs is not None else None,
-        ])
-
-    if CHECK:
+    if CHECK:    # entry mutation checks
         stack.add(write=[
             BM.metadata.FileCheck(),
-            # BM.fields.UrlCheck(),
             # BM.fields.Waybacker(),
+            # BM.files.HashFiles(),
         ])
 
+    if VALIDATE: # entry static checks
+        stack.add(write=[
+            # BM.files.VirusScan(),
+            # BM.fields.UrlCheck(),
+            # BM.metadata.DoiValidator(),
+            # BM.metadata.CrossrefValidator(),
+        ])
+
+
+    if ONLINE:   # online saving
+        stack.add(read=[
+            BM.files.OnlineDownloader(target=_online),
+        ])
+
+    if REPORT:   # stats
+        stack.add(write=[
+            # BM.reporters.SummaryGenerator(),
+        ])
 
     stack.add(read=[BM.failure.FailureHandler()])
     return { _update : stack }
